184f814f0307126bd1ae81553218faabb9f93f56
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-02 15:22:58 -0500

    Create audio_1.js

---
+++ audio_1.js
@@ -0,0 +1,13 @@
+ function PlaySoundAndVideo(soundObj1, videoObj) {
+    var soundAndVideo=document.getElementById(soundObj1,videoObj);
+    soundAndVideo.play();
+}
+        function pauseAudio(){
+    var audio1 = document.getElementById('audio1');
+    var audio2 = document.getElementById('audio2');
+    audio1.pause();
+    audio2.pause();
+}
+        setTimeout(function(){ 
+document.getElementById("audio1").play(); 
+}, 5000)
