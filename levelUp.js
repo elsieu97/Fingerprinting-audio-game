fb66af04923c476bb25b908e539900bcc8895d47
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-03 13:06:15 -0500

    Update levelUp.js

--- levelUp.js
+++ levelUp.js
@@ -11,3 +11,50 @@ function PlaySoundAndVideo2(soundObj3, videoObj) {
         setTimeout(function(){ 
 document.getElementById("audio3").play(); 
 }, 5000)
+//Intializes variables for the next1 round and sends data to server
+//resets variables
+function next1() { 
+	audio1 = null;
+	answer = null;
+	changeScore.style.display1 = "none";
+        background1 = null;
+	document.getElementById("stage").style.display1 = "initial";
+        audio10 = null;
+	startGame();}
+function rounds() {
+	updateScore();
+	play1(audio1);
+	aaaudio1level();
+	if(answered_correctly == true) levelUpPosition("win");
+		else levelUpPosition("lose");
+	scanServer();}
+function scanServer() {
+	scanLevelI();
+	scanUserInput();}
+function scanLevelI() {
+	//Updating info to send to database 
+	var levelInfo = {}; //variable for adding updates info for every round to server
+	levelInfo.name = name1;
+	levelInfo.audio2 = audio2;
+	levelInfo.background5 = background5;
+	levelInfo.background5Pos = order;
+	levelInfo.clickPos = [];
+	levelInfo.answer = answer;
+	levelInfo.userInput = userInput;
+	levelInfo.pointsScored = pointsScored;
+	//send info back to server 	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", servletContext + "/update/", true);
+	xhr.setRequestHeader('Content-Type', 'text/html');
+	xhr.send(JSON.stringify(levelInfo));}
+function scanUserInput() {
+	//Updating info to send to database
+	var jsonMessage = { "messageType" : "continue", "name" : name1, "pointsScored" : highscore };	//continue message
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", servletContext + "/game/", true);
+	xhr.setRequestHeader('Content-Type', 'text/html');
+	//Catches the response and sends it to handler fucntion. Calls for new round.
+	xhr.onload = function () { 							
+		var responseText = this.responseText;
+		handleResponse(responseText);}
+	xhr.send(JSON.stringify(jsonMessage));}
