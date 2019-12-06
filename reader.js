80e0fe1fb59a9b04e53efb5f5ea2941d9b9e7ab9
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-06 13:18:52 -0500

    Update reader.js

--- reader.js
+++ reader.js
@@ -6,6 +6,8 @@ var gsReference = storage.refFromURL('gs://audio2/file.txt')
 // Create a reference from an HTTPS URL
 // Note that in the URL, characters are URL escaped!
 var httpsReference = storage.refFromURL('https://firebasestorage.googleapis.com/v0/b/audio2-692f6.appspot.com/o/file.txt?alt=media&token=07a333ff-6c07-4fa6-9a20-7d9a1f65b542');
+// Get a reference to the database service
+var database = firebase.database();
 storageRef.child('audio2/file.txt').getDownloadURL().then(function(url) {
   // `url` is the download URL for 'audio2/file.txt'
 
