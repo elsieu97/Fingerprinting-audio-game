c3e3d402b1f2b191388613063382e83430be5827
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-08 17:22:45 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+//Listen for form submit
+document.getElementById('question').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+    
+    console.log();
+}
