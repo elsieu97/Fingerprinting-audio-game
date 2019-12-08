d789f82de29857e01dc1546eb98bf0ef8ae3865d
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-08 17:49:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ document.getElementById('question').addEventListener('submit', submitForm);
 function submitForm(e){
     e.preventDefault();
     
-    console.log(123);
+    //get values
+    var question- getInputVal('question');
+}
+
+//function to get values
+function getInputVal(id){
+   return document.getElementById(id).value;
 }
