633ab10b48f5783d1b57dcd9c94e084445a610ec
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-09 00:25:09 -0500

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,12 +1,12 @@
 function every(){
 var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not gonna do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"]; 
-var result = true;
+var result = false;
 var input = document.getElementById("question").value.toLowerCase();
-for(var i=0; i<answers.length; i++){
-  if (answers[i]==input){
-    return = true;
-    break;
+for(var i = 0; i < answers.length; i++){
+  if (input === answers[i].innerhtml){
+    alert("congrats");
   }
+  if(result == false){
+    alert("wrong");
 }
 }
-console.log(result);//false
