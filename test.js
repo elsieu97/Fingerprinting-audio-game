f43412acf03245571a4cb9ca04ea085800f6314d
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-09 14:53:41 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 function test(){
-var b = document.getElementById('score').value, url = ''
-var c = document.getElementById('points').value, url = ''
+var b = document.getElementById('score').value, url = 'https://elsieu97.github.io/Fingerprinting-audio-game/Game2.html?score=' + encodeURIComponent(b);
+var c = document.getElementById('points').value, url = 'https://elsieu97.github.io/Fingerprinting-audio-game/Game2.html?points=' + encodeURIComponent(c);
+document.location.href = url;
+}
