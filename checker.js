e515e2d2fe757c0a1f2f8414cd0924c923343034
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-08 23:54:22 -0500

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,9 +1,12 @@
-$("#btn").click(function(){
+function every(){
 var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not gonna do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"]; 
+var result = true;
 var input = document.getElementById("question").value.toLowerCase();
-var found = answers.find(l => l.answers === input);
-  if (found){
-    alert("Congrats!");
-      } else{
-          alert("Try again");
-      });
+for(var i=0; i<answers.length; i++){
+  if (answers[i]==input){
+    return = true;
+    break;
+  }
+}
+}
+console.log(result);//false
