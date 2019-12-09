a9c37108f137d3574a4b21abdfa703858787450b
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-08 23:06:27 -0500

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,1 +1,9 @@
-var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", " ", " ", " ", 
+$("#btn").click(function(){
+var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not gonna do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"]; 
+var input = document.getElementById("question").value.toLowerCase();
+var found = answers.find(l => l.answers === input);
+  if (found){
+    
+      
+      }
+      });
