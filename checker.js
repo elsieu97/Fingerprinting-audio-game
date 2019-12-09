e2d3ad3ad36c8f849e28eeaaf13ab2d90e3458ee
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-09 04:20:17 -0500

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,5 +1,7 @@
 var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not gonna do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"];
 var input = document.getElementById("question").value.toLowerCase();
+var score = document.getElementById("score").value;
+var points = document.getElementById("points").value;
 function every(e){
    e.preventDefault();
 var match = answers.indexOf(function(r){
