7abeeda90461e4278b9d923b78957524389605fa
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-09 05:19:47 -0500

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,7 +1,6 @@
 var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not gonna do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"];
 var question = document.getElementById('question').value.toLowerCase();
-function every(e){
-  e.preventDefault();
+function every(){
 var match = answers.indexOf(function(r){
   return question === r;
 });
