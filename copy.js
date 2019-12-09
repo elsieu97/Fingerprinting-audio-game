1a008cb1908cd853e8beee8adb26ac344dca7d9b
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-09 17:27:28 -0500

    Update copy.js

--- copy.js
+++ copy.js
@@ -3,8 +3,6 @@ function every(e){
   e.preventDefault();
 var answers = "no thats not gonna do it";
 var question = document.getElementById('question').value.toLowerCase();
-var score = document.getElementById('score').value;
-var points = document.getElementById('points').value;
 var match = answers.some(function(r){
   return question === r;
 });
@@ -23,7 +21,12 @@ points -= 150;
     document.getElementById("score").innerHTML = score;
     document.getElementById("points").innerHTML = points;
       }
+}
+function tryit(){
+  var score = document.getElementById('score').value;
+  var points = document.getElementById('points').value;
   localStorage.setItem("textvalue", score);
+  return false;
   localStorage.setItem("textvalue", points);
   return false;
 }
