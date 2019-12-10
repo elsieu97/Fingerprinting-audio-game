55d9d0d3e1f8dc283ccf0e2bde553001b0fbd767
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-10 04:54:25 -0500

    Update test.js

--- test.js
+++ test.js
@@ -11,8 +11,8 @@ for (var i = 0, l = params.length; i < l; i++) {
          tmp = params[i].split('=');
          data[tmp[0]] = tmp[1];
     }
-    document.getElementById('score').innerHTML = data.name;
-    document.getElementById('points').innerHTML = data.name;
+    document.getElementById('score').innerHTML = data.n;
+    document.getElementById('points').innerHTML = data.n;
 }
 function encode(string) {
       var stringIsInvalid = string === undefined ||
