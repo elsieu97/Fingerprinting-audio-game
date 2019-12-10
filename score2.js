26b5d5370906856234a2e0b76b647ef6958f3fa5
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-09 20:10:52 -0500

    Update score2.js

--- score2.js
+++ score2.js
@@ -1,7 +1,7 @@
 function every(e){
   e.preventDefault();
 var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not going do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"];
-var question = document.getElementById('question').value.toLowerCase();
+var question = document.getElementById("question").value.toLowerCase();
 var match = answers.indexOf(function(r){
   return question === r;
 });
