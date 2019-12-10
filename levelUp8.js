b466751a6a5be92d3d6ecb938b3e798b572f71b1
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-10 03:08:26 -0500

    Create levelUp8.js

---
+++ levelUp8.js
@@ -0,0 +1,26 @@
+function every(){
+var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not going do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 200;
+points += 200;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game8.html");
+  }else{
+alert("wrong");
+score -= 100;
+points -= 160;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
