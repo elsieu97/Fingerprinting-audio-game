5a530e6b25b3cf2851212ab6b7e1e45c4aeb1707
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-10 05:51:58 -0500

    Update audio_1.js

--- audio_1.js
+++ audio_1.js
@@ -8,6 +8,20 @@
     audio1.pause();
     audio2.pause();
 }
+
+function rewind(){
+var audio1 = document.getElementById('audio1');
+var audio2 = document.getElementById('audio2');
+ audio2.play();
+ audio1.play();
+audio1.currentTime;
+ audio2.currentTime;
+audio1.currentTime = 0;
+ audio2.currentTime = 0;
+audio1.currentTime;
+ audio2.currentTime;
+}
+
 setTimeout(function(){ 
 document.getElementById("audio1").play(); 
 }, 6000)
