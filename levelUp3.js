885f28f9c2ea5b2b592d5daa16475681bd454e35
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-10 10:27:05 -0500

    Update levelUp3.js

--- levelUp3.js
+++ levelUp3.js
@@ -1,11 +1,11 @@
 function every(){
 var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not going do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"];
-var question = document.getElementById("question").value.toLowerCase();
-if(question === ""){
+var question2 = document.getElementById("question2").value.toLowerCase();
+if(question2 === ""){
   alert('Nothing was entered');
   }else{
   var match = answers.some(function(r){
-  return question === r;
+  return question2 === r;
 });
     var score=100;
   var points=0;
