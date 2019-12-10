29975cd5cc1eb03b7b5d38f645c602961ee8a6ac
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-10 05:24:13 -0500

    Update score3.js

--- score3.js
+++ score3.js
@@ -1,5 +1,5 @@
 function getDefault() {
 var trail = '?k=("score")';
-var url = "" + trail;
+var url = "https://elsieu97.github.io/Fingerprinting-audio-game/Game2.html" + trail;
 window.location = url;
 }
