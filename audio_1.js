fe9cec3dbb2af7c00c6f2a8fa9602904af4ff39c
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-10 12:36:22 -0500

    Update audio_1.js

--- audio_1.js
+++ audio_1.js
@@ -12,10 +12,10 @@ function rewind(){
 var audio1 = document.getElementById('audio1');
 var audio2 = document.getElementById('audio2');
  audio2.play();
- audio1.play();
 audio1.currentTime;
  audio2.currentTime;
 audio1.currentTime = 6;
+ audio1.play();
  audio2.currentTime = 0;
 audio1.currentTime;
  audio2.currentTime;
