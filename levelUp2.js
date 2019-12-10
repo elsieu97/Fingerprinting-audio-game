b4ab4a8d3f1f3e7c80ce7c4a0812b6e64073548a
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-09 23:03:07 -0500

    Update levelUp2.js

--- levelUp2.js
+++ levelUp2.js
@@ -1,32 +1,4 @@
 function every(){
-var answers = "no thats not going do it";
-var question = document.getElementById('question').value.toLowerCase();
-if(question === ""){
-  alert('Nothing was entered');
-  }else{
-  var match = answers.some(function(r){
-  return question === r;
-});
-    var score=100;
-  var points=0;
-  if (match){
-alert("congrats");
-score += 200;
-points += 200;
-    document.getElementById("score").innerHTML = score;
-    document.getElementById("points").innerHTML = points;
-    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game2.html");
-  }else{
-alert("wrong");
-score -= 150;
-points -= 150;
-    document.getElementById("score").innerHTML = score;
-    document.getElementById("points").innerHTML = points;
-      }
-}
-}
-
-function every2(){
 var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not going do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"];
 var question = document.getElementById('question').value.toLowerCase();
 if(question === ""){
@@ -51,5 +23,4 @@ points -= 150;
     document.getElementById("score").innerHTML = score;
     document.getElementById("points").innerHTML = points;
       }
-}
-}
+}  }
