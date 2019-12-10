330dc719d48660085abaa85284a2bfbb983fd3fc
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-10 12:27:02 -0500

    Update audio_1.js

--- audio_1.js
+++ audio_1.js
@@ -1,17 +1,13 @@
- function PlaySoundAndVideo(soundObj1, soundObj2, videoObj) {
+ function PlaySoundAndVideo(soundObj1,videoObj) {
     var soundAndVideo=document.getElementById(soundObj1,videoObj);
-  var soundV=document.getElementById(soundObj2);
   soundAndVideo.play();
-soundV.currentTime=6;
-  soundV.play();
 }
-        function pauseAudio(){
+    function pauseAudio(){
     var audio1 = document.getElementById('audio1');
     var audio2 = document.getElementById('audio2');
     audio1.pause();
     audio2.pause();
 }
-
 function rewind(){
 var audio1 = document.getElementById('audio1');
 var audio2 = document.getElementById('audio2');
