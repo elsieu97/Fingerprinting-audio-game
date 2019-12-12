958738e3aa257edeb24c6ef47c323e1a9d90fac2
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-12 10:37:49 -0500

    Update levelUp2.js

--- levelUp2.js
+++ levelUp2.js
@@ -1,5 +1,5 @@
 function every(){
-var answers = ["no thats not gonna do it", "No thats not gonna do it"];
+var answers = ["have a good weekend", "you wanna come in", " you know what im talking", "where are we going", "well talk about it soon", "no thats not going do it", "oh yeah everything is fine", "maybe next time huh", "let me out of here", "ill tell you what i think", "i cant believe i let you", "come on come on"];
 var question = document.getElementById("question").value.toLowerCase();
 if(question === ""){
   alert('Nothing was entered');
