b7215df0c53b4c7db0ad8b5d85a849acbdcbc8e0
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-12 12:22:18 -0500

    Create BonusRound.js

---
+++ BonusRound.js
@@ -0,0 +1,26 @@
+function every(){
+var answers = [ " you know what im talking", "ill tebkjll you what i think", "i cant behuhulieve i let you"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 200;
+points += 200;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Finished.html");
+  }else{
+alert("wrong");
+score -= 100;
+points -= 160;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
