9fa3868aaf202860558137203dc966dd7ea181f6
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-14 19:53:31 -0500

    Update score2.js

--- score2.js
+++ score2.js
@@ -206,3 +206,29 @@ points -= 100;
       }
 }
 }
+function every9(){
+var answers = "you wanna come in";
+var question = document.getElementById("question").value.toLowerCase();
+  if(question === ""){
+  alert('Nothing was entered');
+  }else{
+var match = answers.some(function(r){
+  return question === r;
+});
+  var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 200;
+points += 200;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+  }else{
+alert("wrong");
+score -= 100;
+points -= 100;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}
+}
