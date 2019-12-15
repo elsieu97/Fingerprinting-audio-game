083d0c887e5089802b6ea9f746dbee456a496b32
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-15 13:02:04 -0500

    Update levelUp2.js

--- levelUp2.js
+++ levelUp2.js
@@ -1,5 +1,5 @@
 function every(){
-var answers = ["haves a goods weekend", "you wannas come in", " you know what ims talking", "wheres ares we going", "well talks about it soon", "no thats not gonna do it", "oh yeahs everythings is fine", "maybes next time huh", "let mes out of here", "ill tell you whats i think", "i cants believe i let you", "comes on comes on"];
+var answers = ["no thats not gonna do it","No thats not gonna do it", "NO THATS NOT GONNA DO I"];
 var question = document.getElementById("question").value.toLowerCase();
 if(question === ""){
   alert('Nothing was entered');
@@ -20,6 +20,240 @@ points += 200;
 alert("wrong");
 score -= 150;
 points -= 150;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every2(){
+var answers = ["come on come on", "Come on come on", "COME ON COME ON"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=300;
+  var points=200;
+  if (match){
+alert("congrats");
+score += 200;
+points += 300;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game3.html");
+  }else{
+alert("wrong");
+score -= 250;
+points -= 150;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every3(){
+var answers = ["well talk about it", "Well talk about it," "WELL TALK ABOUT IT"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 500;
+points += 310;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game4.html");
+  }else{
+alert("wrong");
+score -= 220;
+points -= 130;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every4(){
+var answers = ["have a good weekend", "Have a goood weekend", "HAVE A GOOD WEEKEND"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 340;
+points += 330;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game5.html");
+  }else{
+alert("wrong");
+score -= 250;
+points -= 120;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every5(){
+var answers = ["you wanna come in", "You wanna come in", "YOU WANNA COME IN"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 340;
+points += 330;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game6.html");
+  }else{
+alert("wrong");
+score -= 250;
+points -= 120;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every6(){
+var answers = ["maybe next time huh", "Maybe next time huh", "MAYBE NECT TIME HUH"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 240;
+points += 230;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game7.html");
+  }else{
+alert("wrong");
+score -= 150;
+points -= 120;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every7(){
+var answers = ["oh yeah everything is fine", "Oh yeah everything is fine", "OH YEAH EVERYTHING IS FINE"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 210;
+points += 220;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game8.html");
+  }else{
+alert("wrong");
+score -= 140;
+points -= 110;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every8(){
+var answers = ["i cant believe i let you", "I cant believe i let you", " ICANT BELIEVE I LET YOU"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=120;
+  var points=20;
+  if (match){
+alert("congrats");
+score += 240;
+points += 230;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game9.html");
+  }else{
+alert("wrong");
+score -= 150;
+points -= 120;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every9(){
+var answers = ["let me out of here", "Let me out of here", "LET ME OUT OF HERE"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=120;
+  var points=20;
+  if (match){
+alert("congrats");
+score += 250;
+points += 230;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game10.html");
+  }else{
+alert("wrong");
+score -= 130;
+points -= 160;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
+function every10(){
+var answers = ["you know what im talking", "You know what im talking", "YOU KNOW WHAT IM TALKING"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=120;
+  var points=20;
+  if (match){
+alert("congrats");
+score += 2050;
+points += 2390;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/BonusRound.html");
+  }else{
+alert("wrong");
+score -= 730;
+points -= 460;
     document.getElementById("score").innerHTML = score;
     document.getElementById("points").innerHTML = points;
       }
