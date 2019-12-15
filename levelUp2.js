732186723177332dec6a5924e720331c0c171920
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-14 19:22:36 -0500

    Update levelUp2.js

--- levelUp2.js
+++ levelUp2.js
@@ -24,3 +24,29 @@ points -= 150;
     document.getElementById("points").innerHTML = points;
       }
 }  }
+function every2(){
+var answers = ["haves ahcjkn goods weekend", "you whghjjannas come in", " you know whattydydyh ims talking", "wheres achfghdrfes we going", "well tvhvjhgalks hiuiabout it soon", "no thats not jhjhgonna do it", "oh yeahs evvhjhhhberythings is fine", "maybehvhjvhjs next time huh", "let mevjuhs out of here", "ill tell you whbbbjhbats i think", "i cants belijkjneve i let you", "come on come on"];
+var question = document.getElementById("question").value.toLowerCase();
+if(question === ""){
+  alert('Nothing was entered');
+  }else{
+  var match = answers.some(function(r){
+  return question === r;
+});
+    var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 200;
+points += 200;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+    location.replace("https://elsieu97.github.io/Fingerprinting-audio-game/Game3.html");
+  }else{
+alert("wrong");
+score -= 150;
+points -= 150;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}  }
