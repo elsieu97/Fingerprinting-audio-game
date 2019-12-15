a1c4852779b278a9ee9e52fe0fd460c3b9c8b39f
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-14 19:40:44 -0500

    Update score2.js

--- score2.js
+++ score2.js
@@ -102,3 +102,29 @@ points -= 100;
       }
 }
 }
+function every5(){
+var answers = [ "no thats not gonna do it", "oh yeah everything is fine", "maybe next time huh"];
+var question = document.getElementById("question").value.toLowerCase();
+  if(question === ""){
+  alert('Nothing was entered');
+  }else{
+var match = answers.some(function(r){
+  return question === r;
+});
+  var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 500;
+points += 300;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+  }else{
+alert("wrong");
+score -= 105;
+points -= 150;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}
+}
