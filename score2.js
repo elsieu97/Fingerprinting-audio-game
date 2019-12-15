8adb2e4ad076fddeaf3c9775c572da04c6fd25f6
elsieu97 <57448282+elsieu97@users.noreply.github.com>
2019-12-14 19:55:48 -0500

    Update score2.js

--- score2.js
+++ score2.js
@@ -232,3 +232,29 @@ points -= 100;
       }
 }
 }
+function every10(){
+var answers = "let me out of here";
+var question = document.getElementById("question").value.toLowerCase();
+  if(question === ""){
+  alert('Nothing was entered');
+  }else{
+var match = answers.some(function(r){
+  return question === r;
+});
+  var score=100;
+  var points=0;
+  if (match){
+alert("congrats");
+score += 230;
+points += 400;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+  }else{
+alert("wrong");
+score -= 200;
+points -= 120;
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("points").innerHTML = points;
+      }
+}
+}
